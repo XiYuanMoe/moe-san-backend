ebe27c77fb709bb0baa1aa7d63f14e92a359c9b4
ManTou <69822848+ManTouQAQ@users.noreply.github.com>
2024-08-14 00:39:47 +0800

    :tada: init commit

---
+++ src/main/java/online/xyworld/moe/moesanbackend/MoeSanBackendApplication.java
@@ -0,0 +1,13 @@
+package online.xyworld.moe.moesanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeSanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoeSanBackendApplication.class, args);
+    }
+
+}
